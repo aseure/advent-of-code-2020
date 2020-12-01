acebdeba08ade66d71126a1757a03b3dd72e4c6a
Anthony Seure <anthony.seure@gmail.com>
2020-12-01 13:43:29 +0100

    feat(01): implement second solution

--- solve-01.hs
+++ solve-01.hs
@@ -1,8 +1,18 @@
 main = do
   content <- readFile "input-01.txt"
   let ints = map (read::String->Int) (lines content)
-  let entries = findEntries ints
-  print $ uncurry (*) entries
+  let tuple = findTwoEntries ints
+  putStrLn $ show . product . toList $ findTwoEntries ints
+  putStrLn $ show . product . toList3 $ findThreeEntries ints
 
-findEntries :: [Int] -> (Int, Int)
-findEntries l = head [(x, y) | x <- l, y <- l, x+y == 2020]
+findTwoEntries :: [Int] -> (Int, Int)
+findTwoEntries l = head [(x, y) | x <- l, y <- l, x+y == 2020]
+
+findThreeEntries :: [Int] -> (Int, Int, Int)
+findThreeEntries l = head [(x, y, z) | x <- l, y <- l, z <- l, x+y+z == 2020]
+
+toList :: (Int, Int) -> [Int]
+toList (a, b) = [a, b]
+
+toList3 :: (Int, Int, Int) -> [Int]
+toList3 (a, b, c) = [a, b, c]
