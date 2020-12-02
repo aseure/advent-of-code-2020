c476066e38bb4b21f2ff09b2408a87d069bb4527
Anthony Seure <anthony.seure@gmail.com>
2020-12-02 19:54:00 +0100

    feat(02): implement second solution

--- solve-02.hs
+++ solve-02.hs
@@ -1,7 +1,11 @@
+import Control.Applicative
+import Control.Lens
+
 main = do
     content <- readFile "input-02.txt"
-    let nbValid = length $ filter isValid (map parse $ lines content)
-    putStrLn $ show nbValid
+    let parsedLines = map parse $ lines content
+    putStrLn $ show . length $ filter isValid1 parsedLines
+    putStrLn $ show . length $ filter isValid2 parsedLines
 
 parse :: String -> (Policy, String)
 parse s =
@@ -13,12 +17,22 @@ data Policy = Policy Int Int Char
 
 policy :: [Char] -> [Char] -> Policy
 policy repeat char =
-    let min = (read::String->Int) $ takeWhile (/= '-') repeat
-        max = (read::String->Int) $ (reverse $ takeWhile (/= '-') (reverse repeat))
+    let i1 = (read::String->Int) $ takeWhile (/= '-') repeat
+        i2 = (read::String->Int) $ (reverse $ takeWhile (/= '-') (reverse repeat))
         letter = head char
-     in Policy min max letter
+     in Policy i1 i2 letter
 
-isValid :: (Policy, String) -> Bool
-isValid (Policy min max char, password) =
+isValid1 :: (Policy, String) -> Bool
+isValid1 (Policy min max char, password) =
     let n = length (filter (== char) password)
     in min <= n && n <= max
+
+isValid2 :: (Policy, String) -> Bool
+isValid2 (Policy pos1 pos2 char, password) =
+    let c1 = password ^? ix (pos1-1)
+        c2 = password ^? ix (pos2-1)
+        maybeMatch = liftA (== char)
+        maybeDifferent = liftA2 (/=) (maybeMatch c1) (maybeMatch c2)
+     in case maybeDifferent of
+          Just True -> True
+          _ -> False
